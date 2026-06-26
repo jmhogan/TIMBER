a39732055694f4de0570782a73a6d41c528cb592
Cai Hallstrom <cthallstrom@gmail.com>
2026-06-26 10:57:42 -0500

    Moved confusion matrix scripts to the cMatrices folder

--- cMatrices/abcTesting.py
+++ cMatrices/abcTesting.py
@@ -1,9 +1,9 @@
 import os, sys, math, re
-from ROOT import TFile, TTree, TH1D, TH2D, TCanvas, gStyle, gPad
+from ROOT import TFile, TTree, TH1D, TH2D, TCanvas, gStyle, gPad, TLatex
 
 readFile = True
 if readFile:
-    file_str = "RDF_TprimeTprime_Par-M-1700_TuneCP5_13p6TeV_amcatnlo-pythia8_2024_0.root"
+    file_str = "root://cmseos.fnal.gov//store/user/lpchtop/TTBB_Jun2026_Run3//RDF_TprimeTprime_Par-M-1900_TuneCP5_13p6TeV_amcatnlo-pythia8_2024_0.root"
     inFile = TFile.Open(file_str)
 
     pattern = r"RDF_([TB]).*?Par-M-(\d+)"
@@ -11,9 +11,9 @@ if readFile:
     name = f"{mass.group(1)}{mass.group(2)}GeV"
 
     truth = TH2D("jet_truth",";tagger ID;true ID",6,0,6,6,0,6)
-    PNWM = TH2D("jet_PNWMid",f";{name} M PNWM ID;true ID",6,0,6,6,0,6)
-    GPT = TH2D("jet_GPTid",f";{name} M GPT ID;true ID",6,0,6,6,0,6)
-    GPTWM = TH2D("jet_GPTWMid",f";{name} M a = 0.5, b = 1.85, c = 2.8 GPTWM ID;true ID",6,0,6,6,0,6)
+    PNWM = TH2D("jet_PNWMid",f";{name} L PNWM ID;true ID",6,0,6,6,0,6)
+    GPT = TH2D("jet_GPTid",f";{name} L GPT ID;true ID",6,0,6,6,0,6)
+    GPTWM = TH2D("jet_GPTWMid",f";{name} L GPTWM ID;true ID",6,0,6,6,0,6)
 
     truth.Sumw2() #Sum weights squared -> account for uncertainties. in future we divide this
     PNWM.Sumw2()
@@ -24,6 +24,7 @@ if readFile:
 
     GPTWMmin = 1000
     BTagM = 0.1272
+    BTagL = 0.0246
 
     a = 0.5
     b = 1.85
@@ -62,7 +63,7 @@ if readFile:
             elif PNWMmaxindex == 4:
                 subjetIdx1 = int(t.gcFatJet_subJetIdx1[ijet])
                 subjetIdx2 = int(t.gcFatJet_subJetIdx2[ijet])
-                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagM) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagM)):
+                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagL) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagL)):
                     PNWMid = 5
                 else:
                     PNWMid = 0
@@ -83,7 +84,7 @@ if readFile:
             elif GPTmaxindex == 3:
                 subjetIdx1 = int(t.gcFatJet_subJetIdx1[ijet])
                 subjetIdx2 = int(t.gcFatJet_subJetIdx2[ijet])
-                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagM) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagM)):
+                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagL) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagL)):
                     GPTid = 5
                 else:
                     GPTid = 0
@@ -104,7 +105,7 @@ if readFile:
             else:
                 subjetIdx1 = int(t.gcFatJet_subJetIdx1[ijet])
                 subjetIdx2 = int(t.gcFatJet_subJetIdx2[ijet])
-                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagM) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagM)):
+                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagL) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagL)):
                     GPTWMid = 5
                 else:
                     GPTWMid = 0
@@ -177,8 +178,6 @@ if readFile:
         for y in range(0,6):
             if x != y:
                 GPTWMsum += GPTWM.GetBinContent(GPTWM.FindBin(x, y)) ** 2
-            else:
-                print(f"{GPTWM.GetBinContent(PNWM.FindBin(x, y))}, ", end = " ")
 
     GPTWMdiag = math.sqrt(GPTWMsum)
 
@@ -193,7 +192,7 @@ if readFile:
 
 
 
-    histFile = TFile.Open("conMats_TpTp_M1700.root", "recreate")
+    histFile = TFile.Open(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L.root", "recreate")
 
     PNWM.Write()
     GPT.Write()
@@ -203,7 +202,7 @@ if readFile:
     histFile.Close()
 
 ## Read histograms from file
-histFile = TFile.Open("conMats_TpTp_M1700.root")
+histFile = TFile.Open(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L.root")
 
 PNWM = histFile.Get("jet_PNWMid")
 GPT = histFile.Get("jet_GPTid")
@@ -223,19 +222,46 @@ for ibin in range(1,GPT.GetNbinsY()+1):
     PNWM.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
     GPTWM.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
 
-# PNWM.SetTitle(f"{name}_PNWM")
-# GPT.SetTitle(f"{name}_GPT")
-# GPTWM.SetTitle(f"{name}_GPTWM")
+PNWM.SetMinimum(0.0)
+PNWM.SetMaximum(1.0)
+GPT.SetMinimum(0.0)
+GPT.SetMaximum(1.0)
+GPTWM.SetMinimum(0.0)
+GPTWM.SetMaximum(1.0)
 
 gStyle.SetOptStat(0)
 gStyle.SetPaintTextFormat("1.2f")
 canv1.SetLeftMargin(0.15)
     
 PNWM.Draw("colz texte")
-canv1.SaveAs(f"{name}_M_PNWM.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_PNWM.png")
 
 GPT.Draw("colz texte")
-canv1.SaveAs(f"{name}_M_GPT.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_GPT.png")
 
 GPTWM.Draw("colz texte")
-canv1.SaveAs(f"{name}_M_abc_GPTWM.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_GPTWM.png")
--- cMatrices/confusionPlots.py
+++ cMatrices/confusionPlots.py
@@ -1,24 +1,27 @@
 import os, sys, math, re
-from ROOT import TFile, TTree, TH1D, TH2D, TCanvas, gStyle, gPad
+from ROOT import TFile, TTree, TH1D, TH2D, TCanvas, gStyle, gPad, TLatex
 
 readFile = True
 if readFile:
-    file_str = "RDF_TprimeTprime_Par-M-1700_TuneCP5_13p6TeV_amcatnlo-pythia8_2024_0.root"
+    file_str = "root://cmseos.fnal.gov//store/user/lpchtop/TTBB_Jun2026_Run3//RDF_TprimeTprime_Par-M-1200_TuneCP5_13p6TeV_amcatnlo-pythia8_2024_0.root"
     inFile = TFile.Open(file_str)
 
     pattern = r"RDF_([TB]).*?Par-M-(\d+)"
     mass = re.search(pattern, file_str)
     name = f"{mass.group(1)}{mass.group(2)}GeV"
 
-    truth = TH2D("jet_truth",";tagger ID;true ID",6,0,6,6,0,6) #need 6 instead
-    PNWM = TH2D("jet_PNWMid",";PNWM ID;true ID",6,0,6,6,0,6)
-    GPT = TH2D("jet_GPTid",";GPT ID;true ID",6,0,6,6,0,6)
-    GPTWM = TH2D("jet_GPTWMid",";GPTWM ID;true ID",6,0,6,6,0,6)
+    truth = TH2D("jet_truth",";tagger ID;true ID",6,0,6,6,0,6)
+    PNWM = TH2D("jet_PNWMid",f";{name} L PNWM ID;true ID",6,0,6,6,0,6)
+    GPT = TH2D("jet_GPTid",f";{name} L GPT ID;true ID",6,0,6,6,0,6)
+    GPTWM = TH2D("jet_GPTWMid",f";{name} L GPTWM ID;true ID",6,0,6,6,0,6)
 
     truth.Sumw2() #Sum weights squared -> account for uncertainties. in future we divide this
     PNWM.Sumw2()
     GPT.Sumw2()
     GPTWM.Sumw2()
+
+    BTagM = 0.1272
+    BTagL = 0.0246
 
     t = inFile.Get("Events_Nominal")
 
@@ -32,7 +35,27 @@ if readFile:
 
             PNWMid = t.gcFatJet_PNWMtags[ijet]
             GPTid = t.gcFatJet_GPTtags[ijet]
-            GPTWMid = t.gcFatJet_GPTWMtags[ijet]
+            
+            GPTWMscores = [t.gcFatJet_GPTWM_ToQCD[ijet], t.gcFatJet_GPTWM_WoQCD[ijet], t.gcFatJet_GPTWM_ZoQCD[ijet]]
+            GPTWMmaxindex = GPTWMscores.index(max(GPTWMscores))
+
+            if GPTWMmaxindex == 0 and max(GPTWMscores) > 1:
+                GPTWMid = 6
+            elif GPTWMmaxindex == 1 and max(GPTWMscores) > 1:
+                GPTWMid = 24
+            elif GPTWMmaxindex == 2 and max(GPTWMscores) > 1:
+                if t.gcFatJet_GPT_regressedMass[ijet] < 105:
+                    GPTWMid = 23
+                else:
+                    GPTWMid = 25
+
+            else:
+                subjetIdx1 = int(t.gcFatJet_subJetIdx1[ijet])
+                subjetIdx2 = int(t.gcFatJet_subJetIdx2[ijet])
+                if((subjetIdx1 >= 0 and t.mySubJet_btag[subjetIdx1] >= BTagL) or (subjetIdx2 >= 0 and t.mySubJet_btag[subjetIdx2] >= BTagL)):
+                    GPTWMid = 5
+                else:
+                    GPTWMid = 0
 
             # Fill truth info into all x-axis values
             if t.gcFatJet_truth[ijet] == 0:   #deciding on the truth, we can just look at the truth from our truth vector
@@ -97,7 +120,7 @@ if readFile:
     GPT.Divide(GPT, truth, 1, 1, "B")
     GPTWM.Divide(GPTWM, truth, 1, 1, "B")
 
-    histFile = TFile.Open("confusionPlots_BpBp.root", "recreate")
+    histFile = TFile.Open(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L.root", "recreate")
 
     PNWM.Write()
     GPT.Write()
@@ -107,7 +130,7 @@ if readFile:
     histFile.Close()
 
 ## Read histograms from file
-histFile = TFile.Open("confusionPlots_BpBp.root")
+histFile = TFile.Open(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L.root")
 
 PNWM = histFile.Get("jet_PNWMid")
 GPT = histFile.Get("jet_GPTid")
@@ -127,19 +150,47 @@ for ibin in range(1,GPT.GetNbinsY()+1):
     PNWM.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
     GPTWM.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
 
-PNWM.SetTitle(f"{name}_PNWM")
-GPT.SetTitle(f"{name}_GPT")
-GPTWM.SetTitle(f"{name}_GPTWM")
     
+PNWM.SetMinimum(0.0)
+PNWM.SetMaximum(1.0)
+GPT.SetMinimum(0.0)
+GPT.SetMaximum(1.0)
+GPTWM.SetMinimum(0.0)
+GPTWM.SetMaximum(1.0)
+
 gStyle.SetOptStat(0)
 gStyle.SetPaintTextFormat("1.2f")
 canv1.SetLeftMargin(0.15)
     
 PNWM.Draw("colz texte")
-canv1.SaveAs(f"{name}_PNWM.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_PNWM.png")
 
 GPT.Draw("colz texte")
-canv1.SaveAs(f"{name}_GPT.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_GPT.png")
 
 GPTWM.Draw("colz texte")
-canv1.SaveAs(f"{name}_GPTWM.png")
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.SetTextAlign(11)
+latex.DrawLatex(0.10, 0.92, "#bf{Private work} (CMS simulation)")
+latex.SetTextAlign(31)
+latex.DrawLatex(0.9, 0.92, "13.6 TeV")
+gPad.Update()
+canv1.SaveAs(f"/uscms_data/d3/cai/run3VLQ/TIMBER/cMatrices/{name}_L_abc_GPTWM.png")
