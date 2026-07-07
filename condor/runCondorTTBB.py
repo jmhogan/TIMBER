334316e4a87fba236fc8460bff1f7496c6f8cb8a
jmhogan <jmhogan@fnal.gov>
2026-07-07 15:18:20 -0500

    unknown pre-merge changes

--- condor/runCondorTTBB.py
+++ condor/runCondorTTBB.py
@@ -10,7 +10,7 @@ exec(open("/uscms_data/d3/jmanagan/EOSSafeUtils.py").read()) # this is a python2
 start_time = time.time()
 
 # --- Sample Dictionary ---
-sample_dic = samples_mc_test
+sample_dic = samples_mc_standard
 #sample_dic = samples_data # This is the name of the list (using list of class objects to keep ordering)
 
 # --- Size of Condor Job ---
